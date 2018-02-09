dca6684c455faae65cf3f0d2aefcad3c50ac0d06
crch <cryptonov@gmail.com>
2018-02-09 14:17:25 +0200

    buttons positioned, js remade

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-//demo fnctions
-function removeCol() {
-    console.log("remove col");
-}
-function addCol() {
-    console.log("add col");
+var container = document.querySelector('.wrapper');
+(function() {
+    console.log(container);
 
+})();
+
+//functions depending on argument adding/removing row/column
+function add(trig) {
+    if(trig){console.log('add col');}
+    else console.log('add row');
+}
+function remove(trig) {
+    if(trig){console.log('remove col');}
+    else console.log('remove row');
 }
