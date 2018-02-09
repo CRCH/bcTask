c24785ab6c24be7bd4b3ea799d30b39e2e8e62fa
crch <cryptonov@gmail.com>
2018-02-09 15:39:41 +0200

    remove buttons added

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 var wrapper = document.querySelector('.wrapper');
+var remCol = document.querySelector('#rem-col');
+var remRow = document.querySelector('#rem-row');
 
 
 (function() {
-
+    var block = document.querySelectorAll('.row .block');
+    block.forEach(function (el) {
+        addEvent(el);
+    });
 
 })();
 
+function addEvent(obj) {
+    obj.addEventListener("mouseover",function () {
+        remCol.classList.toggle('hidden');
+        remRow.classList.toggle('hidden');
+        remCol.style.left = this.offsetLeft-1 + "px";
+        remRow.style.top = this.offsetTop-1+"px";
+
+    });
+    obj.addEventListener("mouseleave",function () {
+        remCol.classList.toggle('hidden');
+        remRow.classList.toggle('hidden');
+    });
+    return obj;
+}
 //functions depending on argument adding/removing row/column
 function add(trig) {
     if(trig){addRow();}
@@ -15,17 +34,20 @@ function remove(trig) {
     if(trig){console.log('remove col');}
     else console.log('remove row');
 }
+
+
 function addRow() {
     var rows = document.querySelector('.wrapper .row');
-    var newRow = rows.cloneNode(true);
+    var newRow = rows.cloneNode(false);
+    for(var i = 0; i < document.querySelectorAll('.row:last-child .block').length; i++)
+        newRow.appendChild(addEvent(document.querySelector('.row .block').cloneNode()));
     wrapper.appendChild(newRow);
 
 }
 function addCol() {
-
     var rows = document.querySelectorAll('.row');
     for(var i = 0; i < rows.length; i++)
-        rows[i].appendChild(document.querySelector('.row .block').cloneNode());
+        rows[i].appendChild(addEvent(document.querySelector('.row .block').cloneNode()));
 
 
 }
