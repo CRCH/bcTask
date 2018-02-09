304d01c8d6e4141d1f9e79f5aa92b4f3848e040f
crch <cryptonov@gmail.com>
2018-02-09 14:50:02 +0200

    col/row add functions added

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-var container = document.querySelector('.wrapper');
+var wrapper = document.querySelector('.wrapper');
+
+
 (function() {
-    console.log(container);
+
 
 })();
 
 //functions depending on argument adding/removing row/column
 function add(trig) {
-    if(trig){console.log('add col');}
-    else console.log('add row');
+    if(trig){addRow();}
+    else addCol();
 }
 function remove(trig) {
     if(trig){console.log('remove col');}
     else console.log('remove row');
+}
+function addRow() {
+    var rows = document.querySelector('.wrapper .row');
+    var newRow = rows.cloneNode(true);
+    wrapper.appendChild(newRow);
+
+}
+function addCol() {
+
+    var rows = document.querySelectorAll('.row');
+    for(var i = 0; i < rows.length; i++)
+        rows[i].appendChild(document.querySelector('.row .block').cloneNode());
+
+
 }
