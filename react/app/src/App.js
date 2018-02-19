8adb07862c1ef62dd486f22590f91887d34bff9a
crch <cryptonov@gmail.com>
2018-02-19 15:06:40 +0200

    css modules try #1

---
+++ react/app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import style from  './comp-table.css';
+class Table extends Component{
+  constructor(){
+    super();
+  }
+}
+class App extends Component {
+  constructor(){
+    super();
+  }
+  render() {
+    return (
+     <div className={style.comp_table__body}>asd</div>
+    );
+  }
+}
+
+export default App;
