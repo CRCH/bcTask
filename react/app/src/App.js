8d0f6dfca6e76d6c886aa3ab05349385c4199199
crch <cryptonov@gmail.com>
2018-03-20 13:32:12 +0200

    stable without css modules

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Table from './Table';
 
+
 const App = () => (
+  <div>
   <Table />
+  <Table />
+  </div>
+  
 );
 
 export default App;
--- react/app/src/Table.js
+++ react/app/src/Table.js
@@ -1,5 +1,7 @@
+import classNames from 'classnames';
 import React, { Component } from 'react';
 import Row from './Row';
+
 
 export default class Table extends Component {
   constructor() {
@@ -79,17 +81,29 @@ export default class Table extends Component {
   }
 
   render() {
+    const remRowClasses = classNames(
+      'block',
+      'button_remove',
+      'button_remrow',
+      { button_hidden: this.state.hiddenRow },
+    );
+    const remColClasses = classNames(
+      'block',
+      'button_remove',
+      'button_remcol',
+      { button_hidden: this.state.hiddenCol },
+    );
     return (
-      <div className="comp_table" onMouseOver={e => this.mouseOver(e)} onMouseOut={e => this.mouseOut(e)} onTouchStart={e => this.mouseOver(e)}>
+      <div className="comp_table" onMouseOver={this.mouseOver.bind(this)} onMouseOut={e => this.mouseOut(e)} onTouchStart={e => this.mouseOver(e)}>
         {this.state.dispR &&
-          <div className={['block button_remove button_remrow', this.state.hiddenRow ? 'button_hidden' : ''].join(' ')} style={{ top: this.state.offTop }} onClick={this.remRow.bind(this)} />}
+          <div className={remRowClasses} style={{ top: this.state.offTop }} onClick={this.remRow.bind(this)} />}
         {this.state.dispC &&
-          <div className={['block button_remove button_remcol', this.state.hiddenCol ? 'button_hidden' : ''].join(' ')} style={{ left: this.state.offLeft }} onClick={this.remCol.bind(this)} />}
+          <div className={remColClasses} style={{ left: this.state.offLeft }} onClick={this.remCol.bind(this)} />}
         <div className="block button_add button_addrow" onClick={this.addRow.bind(this)} />
         <div className="block button_add button_addcol" onClick={this.addCol.bind(this)} />
         <div className="table_body">
-            {this.state.list}
-          </div>
+          {this.state.list}
+        </div>
       </div>
     );
   }
